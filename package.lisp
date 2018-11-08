e1079b08365ea1fe553af597b56010a14e4f6c22
Daniel Keogh <keogh.daniel@gmail.com>
2018-11-08 23:47:13 +1100

    Export useful internals

--- package.lisp
+++ package.lisp
@@ -1,4 +1,12 @@
 (defpackage :ropes
   (:use :cl :iterate :alexandria)
-  (:export #:make-rope #:rope-walk #:rope-concat #:rope-to-string #:rope-elt #:make-rope-iterator
-	   #:do-rope #:rope-substring))
+  (:export
+	 #:make-rope
+	 #:rope-walk
+	 #:rope-concat
+	 #:rope-to-string
+	 #:rope-elt
+	 #:make-rope-iterator
+	 #:do-rope
+	 #:rope-substring
+	 #:rope-node))
